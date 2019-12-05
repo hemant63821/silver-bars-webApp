61b395d73124472132b57490c2efcf0cd7b432a0
hemant63821 <hemant63821@gmail.com>
2019-12-05 17:48:34 +0530

    fix() : removed snackbar

--- src/containers/LiveOrder-Container.jsx
+++ src/containers/LiveOrder-Container.jsx
@@ -3,27 +3,17 @@ import SilverCard from '../components/SilverCard/SilverCard'
 import { getAllSilvers, removeSilverById } from '../redux/actions/allSilverBar-Action'
 import { connect } from 'react-redux';
 import ConfirmDialog from '../components/ConfirmDialog/ConfirmDialog.jsx'
-import MySnackBar from '../utility/SnackBar';
 import _ from 'lodash'
 
 class LiveOrders extends PureComponent {
 
     componentDidMount() {
-        this.props.getAllSilvers()
+        // this.props.getAllSilvers()
     }
-
-    // componentDidUpdate(prevProps) {
-    //     if (_.isEqual(prevProps.silvers, this.props.silvers)) {
-    //         this.setState({
-    //             inventortSaved: true
-    //         })
-    //     }
-    // }
 
     state = {
         openDialogBox: false,
         selectedId: null,
-        inventortSaved: false
     }
 
     openDialog = (id) => {
@@ -54,20 +44,6 @@ class LiveOrders extends PureComponent {
         console.log('check all silvers and check loop', this.props.silvers)
         return (
             <div className="grey-bg">
-                {/* 
-                {
-                    this.state.inventortSaved === true ? (
-                        <MySnackBar
-                            type='success'
-                            open={this.state.inventortSaved}
-                            autoHideDuration={500}
-                            onClose={() => {
-                                this.setState({ inventortSaved: false })
-                            }}
-                            message='Inventory Added SuccessFully'>
-                        </MySnackBar>
-                    ) : null
-                } */}
 
                 {
                     this.state.openDialogBox ? (<ConfirmDialog open={this.state.openDialogBox} handleClose={this.closeDialogBox} onDelete={this.removeSilverData}></ConfirmDialog>) : null
@@ -92,4 +68,4 @@ const mapStateToProps = state => ({
     silvers: state.allSilvers.silverData ? state.allSilvers.silverData : [],
 })
 
-export default connect(mapStateToProps, { getAllSilvers, removeSilverById })(LiveOrders);
+export default connect(mapStateToProps, { removeSilverById })(LiveOrders);
