5f389e741b241b2d58a909ba222acf753c1d15a8
hemant.gogoi <hemant63821@gmail.com>
2021-01-23 11:18:07 +0530

    fix

--- src/containers/testCode-container.jsx
+++ src/containers/testCode-container.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import TextField from '@material-ui/core/TextField';
 // import './RegisterOrder-Container.scss';
 
 class TestCodeContainer extends Component {
 
+    state = {
+        characterCount: 0
+    }
+
+    calculateKeyLength = (e) => {
+        this.setState({
+            characterCount: e.target.value.length
+        })
+    }
     render() {
         return (
             <div className="grey-bg">
-
-                check
+                {
+                    console.log("check length", this.state.characterCount)
+                }
+                <TextField id="outlined-basic" label="Input" variant="outlined" onKeyUp={(e) => this.calculateKeyLength(e)} />
             </div>
         )
     }
